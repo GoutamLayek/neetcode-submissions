1c29445a6d3904a1b666baed1260ccb506637cad
Goutam Layek <142419709+GoutamLayek@users.noreply.github.com>
2026-06-23 22:10:09 +0530

    Add: longest-common-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+
+private:
+    int solve(string text1, string text2, int i, int j, vector<vector<int>>&dp){
+        //base case
+        if(i == text1.size())
+         return 0;
+        
+        if(j == text2.size())
+         return 0;
+        
+        if(dp[i][j] != -1)
+         return dp[i][j];
+        
+        int ans = 0;
+
+        if(text1[i] == text2[j])
+         ans = 1 + solve(text1, text2, i+1, j+1, dp);
+        
+        else
+         ans = max(solve(text1, text2, i+1, j, dp), solve(text1, text2, i, j+1, dp));
+
+        return dp[i][j] = ans;
+    }
+
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        vector<vector<int>>dp(text1.size(), vector<int>(text2.size(), -1));
+        return solve(text1, text2, 0, 0, dp);
+    }
+};
